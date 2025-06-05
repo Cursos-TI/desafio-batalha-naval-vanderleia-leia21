e5f73eb5c1716ca256a1e120032ffc44320d4c39
Vanderleia <vanderleiasantosdaconceicaosan@gmail.com>
2025-06-05 12:25:00 +0000

    criei o posicionamento do primeiro navio

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,5 +4,13 @@ int main() {
 
     int tabuleiro[10][10] = {0}; //Declaração das dimenções do tabuleiro
 
+      int tabuleiro[10][10] = {0}; // Tabuleiro 10x10 preenchido com zeros
+
+   // navio na diagonal 1
+    tabuleiro[0][0] = 3;
+    tabuleiro[1][1] = 3;
+    tabuleiro[2][2] = 3;
+    tabuleiro[3][3] = 3;
+
     return 0;
 }
